364bad0a37420574a54485dd6d57bbc697c31044
dfkeenan <dfkeenan@gmail.com>
2016-11-25 20:18:46 +1000

    Upgraded to Xenko 1.9

--- Source/TilemapTools.Xenko/Xenko/Tiled/XenkoTileLookup.cs
+++ Source/TilemapTools.Xenko/Xenko/Tiled/XenkoTileLookup.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using SiliconStudio.Core.Serialization.Assets;
+using SiliconStudio.Core.Serialization.Contents;
 using SiliconStudio.Xenko.Graphics;
 using TilemapTools.Tiled;
 using TilemapTools.Tiled.Serialization;
